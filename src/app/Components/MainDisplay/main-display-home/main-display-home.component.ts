1f31ce1846a98b4e1e7938c84971e27332324fd2
Benjamin Gyllström <bengy98@live.se>
2022-02-19 11:51:54 +0100

    Home display styling and added some content

--- src/app/Components/MainDisplay/main-display-home/main-display-home.component.ts
+++ src/app/Components/MainDisplay/main-display-home/main-display-home.component.ts
@@ -1,3 +1,5 @@
+import { LogInComponent } from './../../Profile/log-in/log-in.component';
+import { MatDialog } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainDisplayHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+  onSignUp(){
 
+    const dialogRef = this.dialog.open(LogInComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+
+  }
 }
