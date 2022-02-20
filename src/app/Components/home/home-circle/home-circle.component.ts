edf99c37f52873c587b7047f352f119d281abb7e
Benjamin Gyllström <bengy98@live.se>
2022-02-20 15:11:37 +0100

    Home circle changes

--- src/app/Components/home/home-circle/home-circle.component.ts
+++ src/app/Components/home/home-circle/home-circle.component.ts
@@ -8,6 +8,17 @@ import { MatIconRegistry } from '@angular/material/icon';
 })
 export class HomeCircleComponent implements OnInit {
 
+
+  clickedNode: number = 1
+
+  middleSources=[
+    "../../../../assets/images/Qustion1.svg",
+    "../../../../assets/images/Answer1.svg",
+    "../../../../assets/images/social-icon1.png",
+  ]
+  middleSource:string = "../../../../assets/images/Qustion1.svg";
+  middleClicked:boolean
+
   constructor() {
 
   }
@@ -15,8 +26,50 @@ export class HomeCircleComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onClick(node:any){
+  onClick(nodeIndex:any){
+    this.clickedNode = nodeIndex;
+    this.middleClicked = false;
 
+    if(this.clickedNode == 1){
+      this.middleSource = this.middleSources[0];
+    }
+    else if(this.clickedNode == 2){
+      this.middleSource = this.middleSources[2];
+    }
+    else if(this.clickedNode == 3){
+      this.middleSource = this.middleSources[0];
+    }
+    else if(this.clickedNode == 4){
+      this.middleSource = this.middleSources[2];
+    }
+    else if(this.clickedNode == 5){
+      this.middleSource = this.middleSources[0];
+    }
+  }
+  onMiddleClicked(){
+    this.middleClicked = !this.middleClicked;
+
+    if(this.clickedNode == 1){
+      if(this.middleClicked){
+        this.middleSource = this.middleSources[1];
+      }else{
+        this.middleSource = this.middleSources[0];
+      }
+    }
+    if(this.clickedNode == 3){
+      if(this.middleClicked){
+        this.middleSource = this.middleSources[1];
+      }else{
+        this.middleSource = this.middleSources[0];
+      }
+    }
+    if(this.clickedNode == 5){
+      if(this.middleClicked){
+        this.middleSource = this.middleSources[1];
+      }else{
+        this.middleSource = this.middleSources[0];
+      }
+    }
   }
 
   calculateDeg(index:any){
@@ -24,5 +77,12 @@ export class HomeCircleComponent implements OnInit {
     return `rotate(${degree}deg) translate(175px) rotate(-${degree}deg)`;
   }
 
-
+  getBackground(nodeIndex:number){
+    if(nodeIndex == this.clickedNode){
+      return "radial-gradient(80px, #535DC5, transparent 50%)"
+    }
+    else{
+      return "none"
+    }
+  }
 }
