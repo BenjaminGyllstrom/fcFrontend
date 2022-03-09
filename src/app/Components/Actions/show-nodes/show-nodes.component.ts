420fb4698a8812bcc380294e96d87edc137f03fd
Benjamin Gyllström <bengy98@live.se>
2022-03-09 19:27:09 +0100

    node align, line column and rows works

--- src/app/Components/Actions/show-nodes/show-nodes.component.ts
+++ src/app/Components/Actions/show-nodes/show-nodes.component.ts
@@ -51,6 +51,121 @@ export class ShowNodesComponent implements OnInit {
 
   }
 
+  isLastNode(node:any){
+    const index = this.nodes.indexOf(node);
+    return index + 1 == this.nodes.length
+  }
+
+  isEvenRow(node:any){
+    const index = this.nodes.indexOf(node);
+    const rowIndex = Math.floor( index / 3 )
+    const isEvenRow = (rowIndex % 2) == 0;
+    return isEvenRow;
+  }
+
+  getRowIndex(node:any){
+    const index = this.nodes.indexOf(node);
+    return Math.floor( index / 3 )+ 1
+  }
+
+  getColumnIndex(node:any){
+    const index = this.nodes.indexOf(node);
+    return index % 3 + 1;
+  }
+
+  isEndOfRow(node:any){
+    const isEvenRow = this.isEvenRow(node);
+    const columnIndex = this.getColumnIndex(node);
+    // return columnIndex == 2;
+    // console.log(isEvenRow + "/" + columnIndex);
+
+    if(isEvenRow) return columnIndex == 3;
+    else return columnIndex == 1;
+  }
+
+  isStartOfRow(node:any){
+    const isEvenRow = this.isEvenRow(node);
+    const columnIndex = this.getColumnIndex(node);
+    if(isEvenRow) return columnIndex == 1;
+    else return columnIndex == 3;
+  }
+
+  getStartColumn(node:any){
+    const isEvenRow = this.isEvenRow(node);
+    if(isEvenRow){
+      return this.getColumnIndex(node);
+    }else{
+      if(this.isEndOfRow(node)){
+        return this.getColumnIndex(node);
+      }else{
+        return this.getColumnIndex(node) - 1;
+      }
+    }
+  }
+
+  getEndColumn(node:any){
+    const isEvenRow = this.isEvenRow(node);
+
+    if(isEvenRow){
+      if(this.isEndOfRow(node)){
+        return this.getColumnIndex(node);
+      }
+      return this.getColumnIndex(node) + 2;
+    }else{
+      return this.getColumnIndex(node) + 1;
+    }
+  }
+
+  getStartRow(node:any){
+    return this.getRowIndex(node);
+  }
+  getEndRow(node:any){
+
+    if(this.isEndOfRow(node)){
+      return this.getRowIndex(node) + 2;
+    }
+    return this.getRowIndex(node);
+  }
+
+
+  getLineColumn(node:any){
+    this.getColumnIndex(node)
+
+    if(!this.isEvenRow(node)){
+      console.log("column:index"+this.getColumnIndex(node));
+      console.log("evenRow: " +this.isEvenRow(node));
+
+      console.log("column: "+`${this.getStartColumn(node)}/${this.getEndColumn(node)}`);
+    }
+
+    return `${this.getStartColumn(node)}/${this.getEndColumn(node)}`
+
+    // let startIndex = this.getColumn(node);
+    // startIndex = (!this.isEvenRow && !this.isEndOfRow) ? startIndex - 1 : startIndex;
+    // const column = `${startIndex} / span ${this.getLineColumnSpan(node)}`;
+    // return column
+  }
+
+  getLineRow(node:any){
+
+    if(!this.isEvenRow(node)){
+
+      console.log("row: " + `${this.getStartRow(node)}/${this.getEndRow(node)}`);
+      console.log();
+
+      console.log('#####');
+    }
+    return `${this.getStartRow(node)}/${this.getEndRow(node)}`
+  }
+
+  // getLineColumn(node:any){
+  //   let startIndex = this.getColumn(node);
+  //   startIndex = (!this.isEvenRow && !this.isEndOfRow) ? startIndex - 1 : startIndex;
+  //   const column = `${startIndex} / span ${this.getLineColumnSpan(node)}`;
+  //   return column
+  // }
+
+
   // getLineHeight(node:any){
   //   const rowSpan = this.getLineRowSpan(node);
   //   return rowSpan == 2? '50px' : '5px';
