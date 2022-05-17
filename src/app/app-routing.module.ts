64edb5419aace4330dbfe1d15a27c0882d70333d
Benjamin Gyllström <s182842@student.hb.se>
2022-05-17 16:58:08 +0200

    remove old files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,28 +8,14 @@ import { AddCardComponent } from './Components/Actions/add-card/add-card.compone
 import { MyContentComponent } from './Components/my-content/my-content.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CardOverviewComponent } from './Components/Cards/card-overview/card-overview.component';
-import { ChapterOverviewComponent } from './Components/chapters/chapter-overview/chapter-overview.component';
-import { CreateChapterComponent } from './Components/chapters/create-chapter/create-chapter.component';
-import { CreateCardComponent } from './Components/Cards/create-card/create-card.component';
-import { CreateDeckComponent } from './Components/Decks/create-deck/create-deck.component';
 // import { DeckOverviewComponent } from './Components/Decks/deck-overview/deck-overview.component';
-import { EditCardComponent } from './Components/Cards/edit-card/edit-card.component';
 import { HomeComponent } from './Components/home/home.component';
-import { MyDecksComponent } from './Components/Decks/my-decks/my-decks.component';
-import { CreateRootComponent } from './Components/roots/create-root/create-root.component';
-import { MyRootsComponent } from './Components/roots/my-roots/my-roots.component';
-import { RootOverviewComponent } from './Components/roots/root-overview/root-overview.component';
+// import { CreateRootComponent } from './Components/roots/create-root/create-root.component';
+// import { MyRootsComponent } from './Components/roots/my-roots/my-roots.component';
+// import { RootOverviewComponent } from './Components/roots/root-overview/root-overview.component';
 import { StudyComponent } from './Components/study/study.component';
-import { ViewCardsComponent } from './Components/Cards/view-cards/view-cards.component';
-import { ChapterStudyComponent } from './Components/chapters/chapter-study/chapter-study.component';
-import { CreateExplainComponent } from './Components/Explains/create-explain/create-explain.component';
 import { ExplainOverviewComponent } from './Components/Actions/explain-overview/explain-overview.component';
 // import { ExplainOverviewComponent } from './Components/Explains/explain-overview/explain-overview.component';
-import { EditExplainComponent } from './Components/Explains/edit-explain/edit-explain.component';
-import { StudyExplainComponent } from './Components/Explains/study-explain/study-explain.component';
-import { EditDeckComponent } from './Components/Decks/edit-deck/edit-deck.component';
-import { ChapterContentPlaygroundComponent } from './Components/chapters/playground/chapter-content-playground/chapter-content-playground.component';
 import { LogInComponent } from './Components/Profile/log-in/log-in.component';
 import { AuthGuardService } from './Services/AuthGuard.service';
 import { ShowChaptersComponent } from './Components/Actions/show-chapters/show-chapters.component';
@@ -43,8 +29,6 @@ const routes: Routes = [
   {path: 'home', component:HomeComponent},
   {path: 'explore', component:ExploreComponent},
   {path: 'exploreRoot/:rootId', component:ExploreRootOverviewComponent},
-  {path: 'myDecks', component:MyDecksComponent, canActivate: [AuthGuardService]},
-  {path: 'myRoots', component:MyRootsComponent, canActivate: [AuthGuardService]},
   {path: 'myContent', component:MyContentComponent, canActivate: [AuthGuardService], children:[
     {path: 'Roots', component:ShowRootsComponent},
     {path: 'Roots/AddRoot', component:AddRootComponent},
@@ -59,26 +43,10 @@ const routes: Routes = [
     {path:'Roots/:rootId/Chapters/:chapterId/Nodes/Explain/:nodeId/Overview', component: ExplainOverviewComponent},
     {path:'Roots/:rootId/Chapters/:chapterId/Nodes/:nodeType/:nodeId/Study', component: StudyNodeComponent},
   ]},
-  {path: 'createRoot', component:CreateRootComponent, canActivate: [AuthGuardService]},
-  {path: 'rootOverview/:id', component:RootOverviewComponent, canActivate: [AuthGuardService]},
-  {path: 'createChapter/:id', component:CreateChapterComponent, canActivate: [AuthGuardService]},
-  {path: 'chapterOverview/:id', component:ChapterOverviewComponent, canActivate: [AuthGuardService]},
-  {path: 'chapterStudy/:id', component:ChapterStudyComponent, canActivate: [AuthGuardService]},
-  {path: 'chapterPlayground/:id', component:ChapterContentPlaygroundComponent, canActivate: [AuthGuardService]},
-  {path: 'createDeck', component:CreateDeckComponent, canActivate: [AuthGuardService]},
-  {path: 'editDeck/:id', component:EditDeckComponent, canActivate: [AuthGuardService]},
-  {path: 'createExplain/:id', component:CreateExplainComponent, canActivate: [AuthGuardService]},
-  {path: 'createDeck/:id', component:CreateDeckComponent, canActivate: [AuthGuardService]},
   {path: 'deckOverview', component:DeckOverviewComponent, canActivate: [AuthGuardService]},
   {path: 'deckOverview/:id', component:DeckOverviewComponent, canActivate: [AuthGuardService]},
   {path: 'explainOverview/:id', component:ExplainOverviewComponent, canActivate: [AuthGuardService]},
-  {path: 'editExplain/:id', component:EditExplainComponent, canActivate: [AuthGuardService]},
-  {path: 'deckOverview/createCard/:deckId', component:CreateCardComponent, canActivate: [AuthGuardService]},
-  {path: 'deckOverview/viewCards/:deckId', component:ViewCardsComponent, canActivate: [AuthGuardService]},
-  {path: 'cardOverview/:id', component:CardOverviewComponent, canActivate: [AuthGuardService]},
-  {path: 'editCard/:cardId', component:EditCardComponent, canActivate: [AuthGuardService]},
   {path: 'study/:type/:id', component:StudyComponent, canActivate: [AuthGuardService]},
-  {path: 'studyExplain/:id', component:StudyExplainComponent, canActivate: [AuthGuardService]},
   {path: 'login', component:LogInComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,56 +17,24 @@ import {DragDropModule} from '@angular/cdk/drag-drop';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
-import { DeckListOverviewComponent } from './Components/Decks/deck-list-overview/deck-list-overview.component';
-import { CreateDeckComponent } from './Components/Decks/create-deck/create-deck.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { DeckOverviewComponent } from './Components/Actions/deck-overview/deck-overview.component';
-import { CreateCardComponent } from './Components/Cards/create-card/create-card.component';
-import { ViewCardsComponent } from './Components/Cards/view-cards/view-cards.component';
-import { CardListOverviewComponent } from './Components/Cards/card-list-overview/card-list-overview.component';
-import { CardOverviewComponent } from './Components/Cards/card-overview/card-overview.component';
 import { EditCardComponent } from './Components/Actions/show-cards/edit-card/edit-card.component';
 import { StudyComponent } from './Components/study/study.component';
-import { MyDecksComponent } from './Components/Decks/my-decks/my-decks.component';
-import { RootListOverviewComponent } from './Components/roots/root-list-overview/root-list-overview.component';
-import { MyRootsComponent } from './Components/roots/my-roots/my-roots.component';
-import { CreateRootComponent } from './Components/roots/create-root/create-root.component';
-import { RootOverviewComponent } from './Components/roots/root-overview/root-overview.component';
-// import { ChapterListItemComponent } from './Components/chapters/chapter-list-item/chapter-list-item.component';
 import { ChapterListItemComponent } from './Components/Actions/show-chapters/chapter-list-item/chapter-list-item.component';
-import { ChapterOverviewComponent } from './Components/chapters/chapter-overview/chapter-overview.component';
-import { CreateChapterComponent } from './Components/chapters/create-chapter/create-chapter.component';
-import { ChapterStudyComponent } from './Components/chapters/chapter-study/chapter-study.component';
 import { DeckNodeComponent } from './Components/Nodes/deck-node/deck-node.component';
 import { ExplainNodeComponent } from './Components/Nodes/explain-node/explain-node.component';
-import { ExplainListOverviewComponent } from './Components/Explains/explain-list-overview/explain-list-overview.component';
-import { CreateExplainComponent } from './Components/Explains/create-explain/create-explain.component';
-// import { ExplainOverviewComponent } from './Components/Explains/explain-overview/explain-overview.component';
 import { ExplainOverviewComponent } from './Components/Actions/explain-overview/explain-overview.component';
-import { EditExplainComponent } from './Components/Explains/edit-explain/edit-explain.component';
 import { StudyExplainComponent } from './Components/Actions/study-node/study-explain/study-explain.component';
-// import { StudyExplainComponent } from './Components/Explains/study-explain/study-explain.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { EditDeckComponent } from './Components/Decks/edit-deck/edit-deck.component';
 import { MainDisplayComponent } from './Components/MainDisplay/main-display/main-display.component';
 import { MainDisplayHomeComponent } from './Components/MainDisplay/main-display-home/main-display-home.component';
 import { MainDisplayRootsComponent } from './Components/MainDisplay/main-display-roots/main-display-roots.component';
 import { MainDisplayChaptersComponent } from './Components/MainDisplay/main-display-chapters/main-display-chapters.component';
-import { ChapterContentPlaygroundComponent } from './Components/chapters/playground/chapter-content-playground/chapter-content-playground.component';
-import { PlaygroundTemplatesComponent } from './Components/chapters/playground/playground-templates/playground-templates.component';
-import { PlaygroundNodesComponent } from './Components/chapters/playground/playground-nodes/playground-nodes.component';
-import { PlaygroundNodeInfoComponent } from './Components/chapters/playground/playground-node-info/playground-node-info.component';
-import { CreateDeckPlaygroundComponent } from './Components/Decks/create-deck-playground/create-deck-playground.component';
-import { PlaygroundCreateComponent } from './Components/chapters/playground/playground-create/playground-create.component';
-import { CreateCardPlaygroundComponent } from './Components/Cards/create-card-playground/create-card-playground.component';
-import { CreateExplainPlaygroundComponent } from './Components/Explains/create-explain-playground/create-explain-playground.component';
-import { EditDeckPlaygroundComponent } from './Components/Decks/edit-deck-playground/edit-deck-playground.component';
-import { EditExplainPlaygroundComponent } from './Components/Explains/edit-explain-playground/edit-explain-playground.component';
 import { LogInComponent } from './Components/Profile/log-in/log-in.component';
 import { GoogleLoginProvider, SocialAuthService, SocialLoginModule } from 'angularx-social-login';
 import {AuthGuardService} from './Services/AuthGuard.service';
-import { ViewCardsPlaygroundComponent } from './Components/Cards/view-cards-playground/view-cards-playground.component';
 import { Study2Component } from './Components/study2/study2.component';
 import { StudyDueTimerComponent } from './Components/study2/study-due-timer/study-due-timer.component';
 import { CardStudyComponent } from './Components/study2/card-study/card-study.component';
@@ -121,49 +89,20 @@ import { RouterEffects } from './ngrx/router/router.effects';
   declarations: [
     AppComponent,
     HomeComponent,
-    DeckListOverviewComponent,
-    CreateDeckComponent,
     HeaderComponent,
     DeckOverviewComponent,
-    CreateCardComponent,
-    ViewCardsComponent,
-    CardListOverviewComponent,
-    CardOverviewComponent,
     EditCardComponent,
     StudyComponent,
-    MyDecksComponent,
-    RootListOverviewComponent,
-    MyRootsComponent,
-    CreateRootComponent,
-    RootOverviewComponent,
     ChapterListItemComponent,
-    ChapterOverviewComponent,
-    CreateChapterComponent,
-    ChapterStudyComponent,
     DeckNodeComponent,
     ExplainNodeComponent,
-    ExplainListOverviewComponent,
-    CreateExplainComponent,
     ExplainOverviewComponent,
-    EditExplainComponent,
     StudyExplainComponent,
-    EditDeckComponent,
     MainDisplayComponent,
     MainDisplayHomeComponent,
     MainDisplayRootsComponent,
     MainDisplayChaptersComponent,
-    ChapterContentPlaygroundComponent,
-    PlaygroundTemplatesComponent,
-    PlaygroundNodesComponent,
-    PlaygroundNodeInfoComponent,
-    CreateDeckPlaygroundComponent,
-    PlaygroundCreateComponent,
-    CreateCardPlaygroundComponent,
-    CreateExplainPlaygroundComponent,
-    EditDeckPlaygroundComponent,
-    EditExplainPlaygroundComponent,
     LogInComponent,
-    ViewCardsPlaygroundComponent,
     Study2Component,
     StudyDueTimerComponent,
     CardStudyComponent,
