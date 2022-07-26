728971a7aa6a4512c03b6c04d97e8b8d1e646828
Benjamin Gyllström <s182842@student.hb.se>
2022-07-26 14:54:43 +0200

    explore redesign

---
+++ src/app/features/explore/ui/explore-list-headers/explore-list-headers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-explore-list-headers',
+  templateUrl: './explore-list-headers.component.html',
+  styleUrls: ['./explore-list-headers.component.scss']
+})
+export class ExploreListHeadersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
