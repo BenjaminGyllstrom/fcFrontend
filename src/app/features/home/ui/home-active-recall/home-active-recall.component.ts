90e55c1fba76ee99c699425600356eb745a569c8
Benjamin Gyllström <s182842@student.hb.se>
2022-08-12 11:32:13 +0200

    home-card question

--- src/app/features/home/ui/home-active-recall/home-active-recall.component.ts
+++ src/app/features/home/ui/home-active-recall/home-active-recall.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeActiveRecallComponent implements OnInit {
 
+  showAnswer = false;
+
   constructor() { }
 
   ngOnInit(): void {
