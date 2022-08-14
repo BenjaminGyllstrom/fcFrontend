f4aa06a834fe47fe5b9891ee80be2189dc7ef20f
Benjamin Gyllström <s182842@student.hb.se>
2022-08-14 08:35:31 +0200

    fix star-background

---
+++ src/app/features/home/ui/star-background/star-background.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-star-background',
+  templateUrl: './star-background.component.html',
+  styleUrls: ['./star-background.component.scss']
+})
+export class StarBackgroundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
